0fec6f2855412efdefeb5810b86a4315379e0c40
fran-989 <soporte2@viviendasroca.com.ar>
2014-06-01 22:23:15 -0300

    Alta de autores

--- CookBooks.sql
+++ CookBooks.sql
@@ -30,7 +30,7 @@ CREATE TABLE `autores` (
   `apellido` varchar(45) DEFAULT NULL,
   `nacionalidad` varchar(45) DEFAULT NULL,
   PRIMARY KEY (`idautor`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -39,6 +39,7 @@ CREATE TABLE `autores` (
 
 LOCK TABLES `autores` WRITE;
 /*!40000 ALTER TABLE `autores` DISABLE KEYS */;
+INSERT INTO `autores` VALUES (1,'Gato','Dumas','Argentino'),(2,'Petrona','Gandulfo','Argentina');
 /*!40000 ALTER TABLE `autores` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -58,7 +59,7 @@ CREATE TABLE `libro_autor` (
   KEY `id_autor_idx` (`idAutor`),
   CONSTRAINT `id_libro` FOREIGN KEY (`idLibro`) REFERENCES `libros` (`idlibros`) ON DELETE NO ACTION ON UPDATE NO ACTION,
   CONSTRAINT `id_autor` FOREIGN KEY (`idAutor`) REFERENCES `autores` (`idautor`) ON DELETE NO ACTION ON UPDATE NO ACTION
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -67,6 +68,7 @@ CREATE TABLE `libro_autor` (
 
 LOCK TABLES `libro_autor` WRITE;
 /*!40000 ALTER TABLE `libro_autor` DISABLE KEYS */;
+INSERT INTO `libro_autor` VALUES (1,1,1),(4,4,2);
 /*!40000 ALTER TABLE `libro_autor` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -86,7 +88,7 @@ CREATE TABLE `libros` (
   `cantidad` int(11) DEFAULT NULL,
   `fecha_ingreso` datetime DEFAULT NULL,
   PRIMARY KEY (`idlibros`)
-) ENGINE=InnoDB AUTO_INCREMENT=27 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=28 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -95,7 +97,7 @@ CREATE TABLE `libros` (
 
 LOCK TABLES `libros` WRITE;
 /*!40000 ALTER TABLE `libros` DISABLE KEYS */;
-INSERT INTO `libros` VALUES (1,'hola','fdsdf','sdf',10,145,'2014-05-10 00:00:00'),(2,'nombre','tema','edit',20,15,'2014-05-15 00:00:00'),(3,'hola','fdsdf','sdf',10,145,'2014-05-20 00:00:00'),(4,'libro','salud','genesis',100,25,'2014-05-31 01:59:32'),(7,'libro','salud','genesis',100,25,'2014-05-31 01:59:32'),(8,'fsd','sdfsd','dfs',10,50,'2014-05-31 02:38:23'),(9,'tyrty','tyrty','rtyr',10,50,'2014-05-31 02:43:45'),(10,'uyu','tyuty','tyut',10,50,'2014-05-31 02:46:45'),(11,'fgdfg','dfgdfg','dfgdfg',10,50,'2014-05-31 03:08:19'),(12,'uiouio','iouio','uiouio',10,50,'2014-05-31 03:15:07'),(13,'prod1','salud','adit1',100,50,'2014-05-31 14:17:00'),(14,'libro','cocina francesa','conireros',500,5,'2014-05-31 14:18:00'),(26,'libro2','coc inglesa','guadius',100,10,'2014-05-31 14:19:00');
+INSERT INTO `libros` VALUES (1,'hola','fdsdf','sdf',10,145,'2014-05-10 00:00:00'),(4,'libro','salud','genesis',100,25,'2014-05-31 01:59:32'),(7,'libro','salud','genesis',100,25,'2014-05-31 01:59:32'),(8,'fsd','sdfsd','dfs',10,50,'2014-05-31 02:38:23'),(9,'tyrty','tyrty','rtyr',10,50,'2014-05-31 02:43:45'),(10,'uyu','tyuty','tyut',10,50,'2014-05-31 02:46:45'),(11,'fgdfg','dfgdfg','dfgdfg',10,50,'2014-05-31 03:08:19'),(13,'prod1','salud','adit1',100,50,'2014-05-31 14:17:00'),(14,'libro','cocina francesa','conireros',500,5,'2014-05-31 14:18:00'),(26,'libro2','coc inglesa','guadius',100,10,'2014-05-31 14:19:00');
 /*!40000 ALTER TABLE `libros` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -108,4 +110,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2014-05-31 14:49:32
+-- Dump completed on 2014-06-01 17:58:29
